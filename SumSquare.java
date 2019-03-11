a2fd9f7909641cc7e9e62c618d84634fc88abd47
lesterhunn <48440232+lesterhunn@users.noreply.github.com>
2019-03-11 09:01:11 -0400

    Create SumSquare.java

---
+++ SumSquare.java
@@ -0,0 +1,4 @@
+public class SumSquare {
+  public static void main(String[] args) {
+  java.util.Scanner inp = new java.util.Scanner(System.in);	
+  System.out.println("Your program will generate an output of Sum of Squares of multiple of 3" + '\nfrom 3 to 33.");
